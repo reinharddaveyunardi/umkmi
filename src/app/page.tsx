a4aa451984999e78493354d40fca7cf6ba2ac17e
Jeyss <149247347+JeyssV@users.noreply.github.com>
2025-11-13 22:53:14 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className="w-full bg-[#171717]/70 backdrop-blur-sm text-white">
       <Hero />
-      <Recommendation />
+      {/* <Recommendation /> */}
     </div>
   );
 }
