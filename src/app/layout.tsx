6c83fae214da417f0d834f753a9b76f1ef33f77c
Jeyss <149247347+JeyssV@users.noreply.github.com>
2025-11-13 22:53:35 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,22 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ProvidersWrapper from "@/components/ProvidersWrapper";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: [
+    "100", // Thin
+    "200", // Extra Light
+    "300", // Light
+    "400", // Regular
+    "500", // Medium
+    "600", // Semi Bold
+    "700", // Bold
+    "800", // Extra Bold
+    "900", // Black
+  ],
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
